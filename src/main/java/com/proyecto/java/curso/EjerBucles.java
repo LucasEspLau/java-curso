99117e54c2d55df44355d672c26fabc582846f9b
LucasEspLau <lukaespinoza12@gmail.com>
2024-11-24 10:27:48 -0500

    se añadio bucles y hashmap

--- src/main/java/com/proyecto/java/curso/EjerBucles.java
+++ src/main/java/com/proyecto/java/curso/EjerBucles.java
@@ -7,4 +7,18 @@ package com.proyecto.java.curso;
  */
 public class EjerBucles {
     
+    public static void main(String[] args) {
+        //4/2=2 residuo 0
+        //2/2=1 residuo 0
+        //6/2=3 residuo 0
+        //7/2=3 residuo 1
+        //9/2=4 residuo 1
+        
+        for(int num=1;num<=20;num++){
+            System.out.println("El valor de num es: "+num);
+            if(num%2==0){
+                System.out.println(num);
+            }
+        }
+    }
 }
