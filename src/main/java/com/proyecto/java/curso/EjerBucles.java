3cfc07a4da06d5f1aa13aa25a5edadb1fa39b2f5
LucasEspLau <lukaespinoza12@gmail.com>
2024-11-24 10:30:12 -0500

    se modifico bucles

--- src/main/java/com/proyecto/java/curso/EjerBucles.java
+++ src/main/java/com/proyecto/java/curso/EjerBucles.java
@@ -6,5 +6,13 @@ package com.proyecto.java.curso;
  * @author lukae
  */
 public class EjerBucles {
-    
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
+    //GENERAR ERROR
 }
